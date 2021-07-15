84d709d22c8a846a022f5f58012aacd7c4c0ecae
100Root <Artem_19971997@mail.ru>
2021-07-15 10:53:28 +0500

    тест

--- app/resources/js/views/Brigade.vue
+++ app/resources/js/views/Brigade.vue
@@ -53,7 +53,7 @@ export default {
             },
             {
                 title: "Смены",
-                href:"/brigade"
+                href:"/brigade",
             }
         ]
     }),
