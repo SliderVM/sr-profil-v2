81db85e7d9a3d60dab5650de84236f17e50953fc
Mikhail Maksimov <maksimov.myu@gmail.com>
2021-09-28 16:20:40 +0500

    Создает миграции связей пользователей и складов

---
+++ app/database/migrations/2021_09_28_111907_create_user_warehouse_type_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserWarehouseTypeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_warehouse_type', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->bigInteger('warehouse_type_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('warehouse_type_id')->references('id')->on('warehouse_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_warehouse_type');
+    }
+}
